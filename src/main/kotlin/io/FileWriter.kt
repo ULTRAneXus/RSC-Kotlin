08a0f8afd0b014a0622d22ca01927166319c1d2e
Simon Greve <simon.greve@tu-dortmund.de>
2022-09-25 18:40:12 +0200

    fix file writer

--- src/main/kotlin/io/FileWriter.kt
+++ src/main/kotlin/io/FileWriter.kt
@@ -13,11 +13,22 @@ class FileWriter {
     fun writeContentToFile(filePath: String, content: List<String>) {
         println("initialising writing file $filePath")
         val file = java.io.File(filePath)
+        file.createNewFile()
         require(file.isFile) { "$filePath is not a file" }
         require(file.canWrite()) { "$filePath is not writable" }
         println("writing to file $filePath")
-        val bufferedFileWriter = file.bufferedWriter()
-        content.forEach { bufferedFileWriter.write(it) }
+        file.bufferedWriter().use { out -> out.write(convertToString(content)) }
         println("finished writing to file $filePath")
     }
+
+    private fun convertToString(content: List<String>): String {
+        require(content.isNotEmpty()) { "content must not be empty" }
+        if (content.size > 1) {
+            var output = content.first()
+            for (i in 1 until content.size)
+                output += "\n" + content[i]
+            return output
+        } else return content.first()
+    }
 }
+
