f6adae483f38ce84cef4401c026c8ce72480273a
Simon Greve <simon.greve@tu-dortmund.de>
2022-09-25 17:27:19 +0200

    refactor file reader

--- src/main/kotlin/io/FileReader.kt
+++ src/main/kotlin/io/FileReader.kt
@@ -3,23 +3,23 @@ package io
 /**
  * Class used for reading the content of files
  */
-class fileReader {
+class FileReader {
 
     /**
      * Uses a [bufferedReader] to read a file from a given filePath
      * @param filePath the file to be read
      * @return returns a List<String> of the file
      */
-    fun getContent(filePath: String):List<String>{
-        println("initialising file $filePath")
+    fun getContent(filePath: String): List<String> {
+        println("initialising reading file $filePath")
         val file = java.io.File(filePath)
-        require(file.isFile){"$filePath is not a file"}
-        require(file.canRead()){"$filePath is not readable"}
+        require(file.isFile) { "$filePath is not a file" }
+        require(file.canRead()) { "$filePath is not readable" }
         println("reading File $filePath")
         val bufferedFileReader = file.bufferedReader()
         val output = mutableListOf<String>()
         bufferedFileReader.lines().forEach { output.add(it) }
-        println("finished reading file $filePath")
+        println("finished reading from file $filePath")
         return output
     }
 }
