ecb9c722dd94b304af4f1603305c82e83fc5c64c
Simon Greve <yolomastersimon@gmail.com>
2022-10-12 23:43:43 +0200

    add binary converter helper class

---
+++ src/main/kotlin/helper/BinaryConverter.kt
@@ -0,0 +1,31 @@
+package helper
+
+import kotlin.math.pow
+
+/**
+ * Class that converts RageScript literals to integers
+ */
+class BinaryConverter {
+
+    /**
+     * converts a given [literal] to an integer value
+     * @param literal String to be converted to integer
+     * @return Integer value of the literal
+     */
+    fun literalToInt(literal: String): Int {
+        var output = 0
+        var segmentedLiteral = literal.toCharArray().toList()
+        segmentedLiteral = segmentedLiteral.drop(1) //remove leading r
+        segmentedLiteral = segmentedLiteral.reversed() //reverse for binary conversion
+        val literalIsNegative = segmentedLiteral.first() == 'r'
+        if (literalIsNegative) segmentedLiteral = segmentedLiteral.drop(1)
+        for (i in segmentedLiteral.indices) { // binary conversion
+            when (segmentedLiteral[i]) {
+                'e' -> output += 2.0.pow(i).toInt()
+                'a' -> {}
+            }
+        }
+        if (literalIsNegative) output *= -1
+        return output
+    }
+}
