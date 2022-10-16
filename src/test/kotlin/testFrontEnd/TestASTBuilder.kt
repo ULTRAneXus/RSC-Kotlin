00443cb450660959dd60a6a453090b272d568a91
Simon Greve <yolomastersimon@gmail.com>
2022-10-16 14:36:57 +0200

    refactor comment print test and add arithmetic test

--- src/test/kotlin/testFrontEnd/TestASTBuilder.kt
+++ src/test/kotlin/testFrontEnd/TestASTBuilder.kt
@@ -1,10 +1,13 @@
 package testFrontEnd
 
+import astComponents.argument.Argument
 import astComponents.argument.LiteralArgument
 import astComponents.argument.VariableArgument
+import astComponents.component.ArithmeticComponent
 import astComponents.component.CommentComponent
 import astComponents.component.PrintComponent
 import astComponents.component.RootComponent
+import astComponents.operator.ArithmeticOperator
 import astComponents.operator.PrintOperator
 import frontEnd.ASTBuilder
 import kotlin.test.Test
@@ -14,40 +17,79 @@ import kotlin.test.assertTrue
 class TestASTBuilder {
 
     @Test
-    fun testBuildAST() {
-        //comment and print
-        val rootComponent1 = RootComponent(mapOf("EE" to 1, "EEE" to 2))
-        val astBuilder1 = ASTBuilder(rootComponent1)
+    fun testBuildASTCommentPrint() {
+        val rootComponent = RootComponent(mapOf("EE" to 1, "EEE" to 2))
+        val astBuilder1 = ASTBuilder(rootComponent)
         astBuilder1.buildAST(
             listOf(
-                listOf("AA", "I", "am", "a", "comment"),
-                listOf("AAA", "EE", "reee"),
-                listOf("AAAA", "EEE", "reaa")
+                listOf("AA", "I", "am", "a", "comment"), //comment
+                listOf("AAA", "EE", "reee"), //print literal
+                listOf("AAAA", "EEE", "reaa") //print interpreted
             )
         )
         //test size
-        assertEquals(rootComponent1.ast.size, 3)
+        assertEquals(3, rootComponent.ast.size)
         //test comment
-        assertTrue(rootComponent1.ast[0] is CommentComponent)
-        val tempComp1 = rootComponent1.ast[0] as CommentComponent
-        assertEquals(tempComp1.content, "I am a comment")
+        assertTrue(rootComponent.ast[0] is CommentComponent)
+        val tempComp1 = rootComponent.ast[0] as CommentComponent
+        assertEquals("I am a comment", tempComp1.content)
         //test print literal
-        assertTrue(rootComponent1.ast[1] is PrintComponent)
-        var tempComp2 = rootComponent1.ast[1] as PrintComponent
-        assertEquals(tempComp2.operator, PrintOperator.LITERAL)
-        assertEquals(tempComp2.arguments, listOf(VariableArgument("EE"), LiteralArgument("reee")))
+        assertTrue(rootComponent.ast[1] is PrintComponent)
+        var tempComp2 = rootComponent.ast[1] as PrintComponent
+        assertEquals(PrintOperator.LITERAL, tempComp2.operator)
+        assertEquals(listOf(VariableArgument("EE"), LiteralArgument("reee")), tempComp2.arguments)
         //test print interpreted
-        assertTrue(rootComponent1.ast[2] is PrintComponent)
-        tempComp2 = rootComponent1.ast[2] as PrintComponent
-        assertEquals(tempComp2.operator, PrintOperator.INTERPRETED)
-        assertEquals(tempComp2.arguments, listOf(VariableArgument("EEE"), LiteralArgument("reaa")))
-        //arithmetic
-//        val rootComponent2 = RootComponent(mapOf("EE" to 1, "EEE" to 2))
-//        val astBuilder2 = ASTBuilder(rootComponent2)
-//        astBuilder2.buildAST(
-//            listOf(
-//                listOf()
-//            )
-//        )
+        assertTrue(rootComponent.ast[2] is PrintComponent)
+        tempComp2 = rootComponent.ast[2] as PrintComponent
+        assertEquals(PrintOperator.INTERPRETED, tempComp2.operator)
+        assertEquals(listOf(VariableArgument("EEE"), LiteralArgument("reaa")), tempComp2.arguments)
+
+    }
+
+    @Test
+    fun testASTArithmetic() {
+        val rootComponent = RootComponent(mapOf("EE" to 1, "EEE" to 2))
+        val astBuilder2 = ASTBuilder(rootComponent)
+        astBuilder2.buildAST(
+            listOf(
+                listOf("AAAAA", "EE", "EE", "EEE"), //add
+                listOf("AAAAAA", "EEE", "ree", "reer"), //subtract
+                listOf("AAAAAAA", "EE", "EE", "rea"), //multiply
+                listOf("AAAAAAAA", "EEE", "reeeea", "EE"), //divide
+                listOf("AAAAAAAAA", "EE", "reee", "ree") //modulo
+            )
+        )
+        //test size
+        assertEquals(rootComponent.ast.size, 5)
+        //test add
+        assertTrue(rootComponent.ast[0] is ArithmeticComponent)
+        var tempComp = rootComponent.ast[0] as ArithmeticComponent
+        assertEquals(ArithmeticOperator.ADD, tempComp.operator)
+        assertEquals(VariableArgument("EE"), tempComp.target)
+        assertEquals(listOf<Argument>(VariableArgument("EE"), VariableArgument("EEE")), tempComp.arguments)
+        //test subtract
+        assertTrue(rootComponent.ast[1] is ArithmeticComponent)
+        tempComp = rootComponent.ast[1] as ArithmeticComponent
+        assertEquals(ArithmeticOperator.SUBTRACT, tempComp.operator)
+        assertEquals(VariableArgument("EEE"), tempComp.target)
+        assertEquals(listOf<Argument>(LiteralArgument("ree"), LiteralArgument("reer")), tempComp.arguments)
+        //test multiply
+        assertTrue(rootComponent.ast[2] is ArithmeticComponent)
+        tempComp = rootComponent.ast[2] as ArithmeticComponent
+        assertEquals(ArithmeticOperator.MULTIPLY, tempComp.operator)
+        assertEquals(VariableArgument("EE"), tempComp.target)
+        assertEquals(listOf(VariableArgument("EE"), LiteralArgument("rea")), tempComp.arguments)
+        //test divide
+        assertTrue(rootComponent.ast[3] is ArithmeticComponent)
+        tempComp = rootComponent.ast[3] as ArithmeticComponent
+        assertEquals(ArithmeticOperator.DIVIDE, tempComp.operator)
+        assertEquals(VariableArgument("EEE"), tempComp.target)
+        assertEquals(listOf(LiteralArgument("reeeea"), VariableArgument("EE")), tempComp.arguments)
+        //test modulo
+        assertTrue(rootComponent.ast[4] is ArithmeticComponent)
+        tempComp = rootComponent.ast[4] as ArithmeticComponent
+        assertEquals(ArithmeticOperator.MODULO, tempComp.operator)
+        assertEquals(VariableArgument("EE"), tempComp.target)
+        assertEquals(listOf<Argument>(LiteralArgument("reee"), LiteralArgument("ree")), tempComp.arguments)
     }
 }
