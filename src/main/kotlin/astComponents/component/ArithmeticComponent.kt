3a13252504172f7516306424fec72e3b8be4526a
Simon Greve <yolomastersimon@gmail.com>
2022-10-25 01:05:42 +0200

    make ArithmeticComponent use VariableComponent as target instead

--- src/main/kotlin/astComponents/component/ArithmeticComponent.kt
+++ src/main/kotlin/astComponents/component/ArithmeticComponent.kt
@@ -1,15 +1,16 @@
 package astComponents.component
 
 import astComponents.argument.Argument
+import astComponents.argument.VariableArgument
 import astComponents.operator.ArithmeticOperator
 
 /**
  * Arithmetic component for building the AST, RageScript equivalent of an arithmetic function
  * @property operator [ArithmeticOperator]
- * @property target Argument Target variable for saving the result
+ * @property target VariableArgument Target variable for saving the result
  * @property arguments MutableList<Argument> Arguments used for the calculation
  */
 data class ArithmeticComponent(val operator: ArithmeticOperator) : Component {
-    lateinit var target: Argument
+    lateinit var target: VariableArgument
     var arguments = mutableListOf<Argument>()
 }
